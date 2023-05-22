e3e14b5abef1eeb6ee0c6e93d3144869089f0c67
stefanluth <stefan.luthardt@outlook.com>
2023-05-22 19:32:06 +0200

    add code of conduct

---
+++ app/code-of-conduct/page.tsx
@@ -0,0 +1,25 @@
+export default function CodeOfConduct() {
+  return (
+    <div className="pt-4">
+      <h1 className="text-3xl font-bold">Verhaltenskodex für den Kummerkasten</h1>
+      <ol className="flex flex-col gap-2 p-2 text-lg list-decimal">
+        <li>Sei respektvoll: Behandele andere mit Höflichkeit und Respekt, unabhängig von ihrer Meinung.</li>
+        <li>Bleibe beim Thema: Halte die Diskussionen relevant und vermeide entgleisende Threads.</li>
+        <li>Keine persönlichen Angriffe: Unterlasse persönliche Angriffe, Belästigungen oder Drohungen.</li>
+        <li>Privatsphäre schützen: Respektiere die Anonymität und gebe keine persönlichen Daten preis.</li>
+        <li>
+          Keine illegalen oder schädlichen Inhalte: Veröffentliche keine illegalen oder schädlichen Inhalte,
+          einschließlich Hassreden oder diskriminierenden Bemerkungen.
+        </li>
+        <li>Verstöße melden: Melde etwaige Verstöße umgehend an die Administratoren.</li>
+        <li>Kein Spam oder Trolling: Vermeide störendes Verhalten wie Spam oder Trolling.</li>
+        <li>Verantwortungsvoller Umgang: Manipuliere keine Diskussionen und verwende keine Bots.</li>
+      </ol>
+      <p className="font-bold">
+        Denke daran, dass der Zweck eines anonymen Message Boards darin besteht, offene Diskussionen und den
+        Austausch von Ideen zu ermöglichen. Durch die Befolgung dieses Verhaltenskodex können wir eine
+        einladende und integrative Gemeinschaft für alle Beteiligten fördern.
+      </p>
+    </div>
+  );
+}
