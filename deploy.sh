c1ac2ad7bf66e89c0653392d0c6ceddcc0f353be
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-05-26 18:34:59 +0200

    add deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build --tag kummerkasten .
+
+WORDS=4
+export UNLOCK_PASSWORD=$(curl -sSf "https://makemeapassword.ligos.net/api/v1/passphrase/plain?pc=1&wc=${WORDS}&maxCh=64" \
+  | sed 's/ /-/g' | tr -d '\r')
+
+
+echo "===================================="
+echo "Password is:"
+echo "===================================="
+echo $UNLOCK_PASSWORD
+echo "===================================="
+
+docker-compose up
