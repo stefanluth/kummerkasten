c786f7a145a3cc4acd426f741214b93e82342c25
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-05-26 12:55:41 +0200

    edit min content length

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const MIN_TITLE_LENGTH = 4;
 export const MAX_TITLE_LENGTH = 128;
-export const MIN_CONTENT_LENGTH = 32;
+export const MIN_CONTENT_LENGTH = 16;
 export const MAX_CONTENT_LENGTH = 1028;
