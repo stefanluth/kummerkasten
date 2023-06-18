014d5c251eac41352187f5763d6ea5aac3d60ce3
stefanluth <stefan.luthardt@outlook.com>
2023-06-18 23:43:39 +0200

    run prettier

--- app/_actions/vote.ts
+++ app/_actions/vote.ts
@@ -1,56 +1,56 @@
-import { prisma } from "@/utils/prisma";
-import { revalidatePath } from "next/cache";
-import { cookies } from "next/headers";
+import { prisma } from '@/utils/prisma';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
 
 export async function vote(formData: FormData) {
-    'use server';
+  'use server';
 
-    const postId = formData.get('postId') as string;
-    const upvote = formData.get('upvote') === 'true';
-    const fingerprint = cookies().get('fingerprint')?.value;
+  const postId = formData.get('postId') as string;
+  const upvote = formData.get('upvote') === 'true';
+  const fingerprint = cookies().get('fingerprint')?.value;
 
-    const post = await prisma.post.findUnique({
-      where: {
-        id: postId,
-      },
-    });
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
 
-    if (!post || !fingerprint) {
-      return;
-    }
-
-    const vote = await prisma.vote.findFirst({
-      where: {
-        postId,
-        fingerprint,
-      },
-    });
-
-    if (vote) {
-      return;
-    }
-
-    await prisma.vote.create({
-      data: {
-        postId,
-        fingerprint,
-      },
-    });
-
-    await prisma.post.update({
-      where: {
-        id: postId,
-      },
-      data: {
-        upvotes: upvote ? post.upvotes + 1 : post.upvotes - 1,
-      },
-    });
-
-    revalidatePath('/');
-    revalidatePath(`/${postId}`);
-    revalidatePath('/top/day');
-    revalidatePath('/top/week');
-    revalidatePath('/top/month');
-    revalidatePath('/top/year');
-    revalidatePath('/top/all');
+  if (!post || !fingerprint) {
+    return;
   }
+
+  const vote = await prisma.vote.findFirst({
+    where: {
+      postId,
+      fingerprint,
+    },
+  });
+
+  if (vote) {
+    return;
+  }
+
+  await prisma.vote.create({
+    data: {
+      postId,
+      fingerprint,
+    },
+  });
+
+  await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      upvotes: upvote ? post.upvotes + 1 : post.upvotes - 1,
+    },
+  });
+
+  revalidatePath('/');
+  revalidatePath(`/${postId}`);
+  revalidatePath('/top/day');
+  revalidatePath('/top/week');
+  revalidatePath('/top/month');
+  revalidatePath('/top/year');
+  revalidatePath('/top/all');
+}
