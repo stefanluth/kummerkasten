f98b362f2bc02fbe0bae2cdff9340788fc1285b2
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-06-19 14:25:04 +0200

    update modal ui

--- app/reported/confirmation.tsx
+++ app/reported/confirmation.tsx
@@ -13,23 +13,21 @@ export default function Confirmation() {
 
   return (
     <Modal title="Gemeldete Nachrichten anzeigen" close={close}>
-      <div className="flex flex-col max-w-lg p-4 gap-2 mx-auto">
-        <form onSubmit={close}>
-          <div className="bg-zinc-700 rounded-lg p-4 flex flex-col items-center">
-            <ExclamationTriangleIcon className="w-6 h-6 text-yellow-500 mb-2" />
-            <p className="text-white mb-2 text-center">
-              Durch das Bestätigen werden gemeldete Nachrichten angezeigt, die möglicherweise als verstörend oder
-              unangebracht empfunden werden können.
-            </p>
-            <button
-              type="submit"
-              className="w-fit h-8 px-2 rounded-md bg-yellow-500 hover:bg-yellow-600 text-zinc-700 font-bold self-center"
-            >
-              Bestätigen
-            </button>
-          </div>
-        </form>
-      </div>
+      <form onSubmit={close}>
+        <div className="flex flex-col items-center">
+          <ExclamationTriangleIcon title="Warnung" className="w-6 h-6 text-yellow-500 mb-2" />
+          <p className="text-white mb-2 text-center">
+            Durch das Bestätigen werden gemeldete Nachrichten angezeigt, die möglicherweise als verstörend oder
+            unangebracht empfunden werden können.
+          </p>
+          <button
+            type="submit"
+            className="w-fit h-8 px-2 rounded-md bg-yellow-500 hover:bg-yellow-600 text-zinc-700 font-bold self-center"
+          >
+            Bestätigen
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 }
