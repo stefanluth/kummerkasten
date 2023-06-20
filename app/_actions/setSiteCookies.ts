390035b7dc94de886904887b87c6ef809ea08254
Stefan Luthardt <stefan.luthardt@outlook.com>
2023-06-20 10:49:42 +0200

    set cookies httponly

--- app/_actions/setSiteCookies.ts
+++ app/_actions/setSiteCookies.ts
@@ -3,25 +3,28 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+type CookieOptions = {
+  path: string;
+  maxAge: number;
+  secure: boolean;
+  sameSite: boolean | 'lax' | 'strict' | 'none' | undefined;
+  httpOnly: boolean;
+};
+
+const cookieOptions: CookieOptions = {
+  path: '/',
+  maxAge: 86400,
+  secure: true,
+  sameSite: 'strict',
+  httpOnly: true,
+};
+
 export async function setSiteCookies(formData: FormData) {
   const password = formData.get('password') as string;
   const fingerprint = formData.get('fingerprint') as string;
 
-  console.log({ password, fingerprint });
-
-  cookies().set('password', password, {
-    path: '/',
-    maxAge: 86400,
-    secure: true,
-    sameSite: 'strict',
-  });
-
-  cookies().set('fingerprint', fingerprint, {
-    path: '/',
-    maxAge: 86400,
-    secure: true,
-    sameSite: 'strict',
-  });
+  cookies().set('password', password, cookieOptions);
+  cookies().set('fingerprint', fingerprint, cookieOptions);
 
   redirect('/');
 }
