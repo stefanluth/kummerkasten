515081d879c8c64b5a022755b398f9453cba0c3f
stefanluth <stefan.luthardt@outlook.com>
2023-08-21 23:30:28 +0200

    update faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -7,13 +7,13 @@ export default function About() {
     <div className="flex flex-col max-w-2xl gap-6 p-2 mx-auto">
       <FaqItem title="Was ist der Kummerkasten?">
         <p>
-          Obwohl eine offene Kommunikationskultur gefördert wird, gibt es manchmal Fragen oder Meinungen, die man
-          vielleicht nicht persönlich äußern möchte, aus Angst vor Konsequenzen oder einem möglichen Unbehagen bei der
-          Diskussion von sensiblen Themen.
+          In Umgebungen, in denen offene Kommunikation geschätzt wird, können dennoch Momente auftreten, in denen einige
+          Fragen oder Ansichten vielleicht nicht direkt ausgedrückt werden wollen. Dies könnte aus Besorgnis über
+          mögliche Reaktionen oder einem Unbehagen bei bestimmten Themen herrühren.
         </p>
         <p>
-          Der Kummerkasten bietet einen sicheren und vertraulichen Raum, in dem Mitarbeiter:innen ihre Gedanken und
-          Meinungen frei teilen können.
+          Der Kummerkasten stellt in solchen Fällen eine vertrauenswürdige Plattform dar, auf der Mitarbeiter:innen ihre
+          Gedanken in einem geschützten Rahmen austauschen können.
         </p>
       </FaqItem>
       <FaqItem title="Wie funktioniert der Kummerkasten?">
@@ -36,7 +36,7 @@ export default function About() {
           Nachricht schreiben und auf <b>Absenden</b> klicken.
         </p>
       </FaqItem>
-      <FaqItem title="Ich sehe keine Nachrichten auf der Startseite.">
+      <FaqItem title="Ich sehe keine Nachrichten. Was muss ich tun?">
         <p>Du benötigst das Passwort, das regelmäßig erneuert wird.</p>
         <p>
           Das Passwort musst du auf der{' '}
@@ -48,14 +48,17 @@ export default function About() {
       </FaqItem>
       <FaqItem title="Woher kriege ich das Passwort?">
         <p>
-          Das Passwort sollte dir von deinem Teamleiter:in oder einer anderen Person, die Zugang zum Kummerkasten hat,
-          mitgeteilt werden.
+          Das Passwort sollte dir einer Person, die Zugang zum Kummerkasten hat, mitgeteilt werden, oder in einem
+          anderen Kommunikationskanal, der für dich zugänglich ist, veröffentlicht werden. So können kann garantiert
+          werden, dass nur berechtigte Personen Zugang erhalten und die Anonymität sowie der Schutz der Anliegen und
+          Gedanken der Mitarbeiter:innen gewahrt bleiben. Es ist wichtig, das Passwort nicht an unbefugte Dritte
+          weiterzugeben.
         </p>
       </FaqItem>
       <FaqItem title="Wie kann ich eine Nachricht melden?">
         <p>
-          Wenn du eine Nachricht melden möchtest, kannst du den Link <b>Melden</b> über der Nachricht verwenden. Wir
-          werden die Nachricht überprüfen und sie entfernen, wenn sie gegen unsere{' '}
+          Wenn du eine Nachricht melden möchtest, kannst du den Link <b>Melden</b> über der Nachricht verwenden. Wird
+          eine Nachricht oft gemeldet, wird sie zunächst ausgeblendet und dann ggf. entfernt, wenn sie gegen die{' '}
           <Link href="/code-of-conduct" className="text-slate-300 underline">
             Verhaltensregeln
           </Link>{' '}
@@ -68,9 +71,9 @@ export default function About() {
           eingesehen werden.
         </p>
       </FaqItem>
-      <FaqItem title="Wie kann ich eine Nachricht löschen?">
+      <FaqItem title="Wie kann ich meine Nachricht löschen?">
         <p>
-          Nachrichten können nicht gelöscht werden. Wenn du eine Nachricht löschen möchtest, kannst du sie melden und
+          Nachrichten können nicht gelöscht werden. Wenn du deine Nachricht löschen möchtest, kannst du sie melden und
           sie wird entfernt, wenn sie gegen die{' '}
           <Link href="/code-of-conduct" className="text-slate-300 underline">
             Verhaltensregeln
@@ -78,8 +81,14 @@ export default function About() {
           verstößt.
         </p>
       </FaqItem>
+      <FaqItem title="Warum kann ich meine Nachricht nicht löschen?">
+        <p>
+          Nachrichten können nicht gelöscht werden, um die Anonymität zu gewährleisten, da nicht nachvollzogen werden
+          kann, wer die Nachricht geschrieben hat.
+        </p>
+      </FaqItem>
       <FaqItem title="Wie kann ich eine Nachricht bearbeiten?">
-        <p>Nachrichten können nicht bearbeitet werden.</p>
+        <p>Nachrichten können aus demselben Grund nicht bearbeitet werden, aus dem sie nicht gelöscht werden können.</p>
       </FaqItem>
       <FaqItem title="Wie kann ich eine Nachricht teilen?">
         <p>
@@ -96,7 +105,8 @@ export default function About() {
       <FaqItem title="Wie kann ich auf eine Nachricht antworten?">
         <p>
           Dieses Feature ist noch nicht verfügbar. Es wird daran gearbeitet, es so schnell wie möglich zu
-          implementieren.
+          implementieren. Bis dahin kannst du die Nachrichten-ID am Ende der Überschrift kopieren, um sie in deiner
+          Antwort zu erwähnen.
         </p>
       </FaqItem>
       <FaqItem title="Wie kann ich sicher sein, dass meine Nachrichten und Bewertungen anonym sind?">
@@ -111,28 +121,28 @@ export default function About() {
           Tool umgehen. Wenn festgestellt wird, dass das nicht der Fall ist, wird das Tool deaktiviert.
         </p>
       </FaqItem>
-      <FaqItem title="Wo kann ich den Source Code finden?">
-        <p>
-          Der Source Code ist auf{' '}
-          <Link href="https://github.com/stefanluth/kummerkasten" className="text-slate-300 underline">
-            GitHub
-          </Link>{' '}
-          verfügbar.
-        </p>
-      </FaqItem>
       <FaqItem title="Im Source Code steht etwas über einen 'Fingerprint'. Was ist das?">
         <p>
-          Der Fingerprint ist ein Hash (eine Art von ID), der aus deiner Browser- und Gerätekonfiguration generiert
-          wird. Diese Informationen selbst werden nicht gespeichert, nur der daraus generierte Hash. Er ermöglicht es
-          uns, die grobe Anzahl der Besucher:innen zu zählen, mehrfaches Abstimmen zu verhindern und Spam zu erkennen
-          ohne IP-Adressen oder andere Daten zu speichern, die Rückschlüsse auf deine Identität zulassen.
+          Der Fingerprint ist eine Art von ID, der aus deiner Browser- und Gerätekonfiguration generiert wird. Diese
+          Informationen selbst werden nicht gespeichert, nur der daraus generierte Hash. Er ermöglicht es uns,
+          mehrfaches Abstimmen zu verhindern und Spam zu erkennen ohne IP-Adressen oder andere Daten zu speichern, die
+          Rückschlüsse auf deine Identität zulassen.
         </p>
       </FaqItem>
-      <FaqItem title="Im Source Code steht etwas über eine IP-Adresse. Wird meine IP-Adresse gespeichert?">
+      <FaqItem title="Aber im Source Code steht etwas über eine IP-Adresse. Wird meine IP-Adresse doch gespeichert?">
         <p>
           Nein, deine IP-Adresse wird nicht gespeichert. Die IP-Adresse wird verwendet, um den Hash zu generieren, der
           als Fingerprint bezeichnet wird. Die IP-Adresse selbst wird nicht gespeichert und kann nicht mit deinen
           Nachrichten oder Bewertungen in Verbindung gebracht werden.
+        </p>
+      </FaqItem>
+      <FaqItem title="Ist der Kummerkasten mit dem Unternehmen XY verbunden?">
+        <p>
+          Nein, der Kummerkasten ist{' '}
+          <Link href="https://github.com/stefanluth/kummerkasten" className="text-slate-300 underline">
+            ein privates Open Source Projekt
+          </Link>
+          . Es ist nicht mit irgendeinem Unternehmen verbunden und wurde nur zum Austausch mit Kolleg:innen entwickelt.
         </p>
       </FaqItem>
     </div>
