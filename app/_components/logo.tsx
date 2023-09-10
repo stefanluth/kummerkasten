dfce9ee57e3ccb39099142d654191756fc11853f
stefanluth <stefan.luthardt@outlook.com>
2023-09-10 13:58:56 +0200

    update logo

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -5,8 +5,8 @@ import logo from '@/app/kummerkasten.png';
 
 export function Logo() {
   return (
-    <Link href="/" title="Home" className="flex w-fit">
-      <Image src={logo} alt="Logo" className="w-12 h-12 rounded-md" />
+    <Link href="/" title="Home" draggable={false} className="flex w-fit select-none">
+      <Image src={logo} alt="Logo" draggable={false} className="w-12 h-12 p-1 select-none" />
     </Link>
   );
 }
