325cc16ee18b2780159f67f926a3401f3965ca73
stefanluth <stefan.luthardt@outlook.com>
2023-11-19 22:43:56 +0100

    update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!unlock|faq|code-of-conduct|api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!unlock|faq|code-of-conduct|api|_next|public|static|favicon.ico).*)'],
 };
