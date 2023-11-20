c30b90e76da7102ac80216480108e7cb9fe707c4
stefanluth <stefan.luthardt@outlook.com>
2023-11-20 16:20:11 +0100

    change loglevel to info, warn, error

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -36,7 +36,7 @@ const globalForPrisma = global as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['info', 'warn', 'error'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
