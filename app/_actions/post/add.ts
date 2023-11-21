f73499a2b9ef90fd223a1b76d87151ee3c7417bd
stefanluth <stefan.luthardt@outlook.com>
2023-11-21 17:59:27 +0100

    add useFormState for server side validation errors

--- app/_actions/post/add.ts
+++ app/_actions/post/add.ts
@@ -13,16 +13,26 @@ export async function addPost(formData: FormData) {
   const password = cookies().get('password')?.value;
   if (password !== process.env.UNLOCK_PASSWORD) return redirect('/unlock');
 
-  const title = formData.get('title') as string;
-  const content = purify(formData.get('content') as string);
+  const rawTitle = formData.get('title') as string;
+  const title = rawTitle.trim();
+
+  const rawContent = formData.get('content') as string;
+  const content = purify(rawContent.trim());
 
   const titleTooShort = title.length < Number(process.env.MIN_TITLE_LENGTH ?? DEFAULTS.MIN_TITLE_LENGTH);
   const titleTooLong = title.length > Number(process.env.MAX_TITLE_LENGTH ?? DEFAULTS.MAX_TITLE_LENGTH);
   const contentTooShort = content.length < Number(process.env.MIN_CONTENT_LENGTH ?? DEFAULTS.MIN_CONTENT_LENGTH);
   const contentTooLong = content.length > Number(process.env.MAX_CONTENT_LENGTH ?? DEFAULTS.MAX_CONTENT_LENGTH);
 
-  if (titleTooShort || titleTooLong || contentTooShort || contentTooLong) {
-    return;
+  switch (true) {
+    case titleTooShort:
+      return 'Titel zu kurz. Überprüfe auf angehängte Leerzeichen.';
+    case titleTooLong:
+      return 'Titel zu lang. Überprüfe auf angehängte Leerzeichen.';
+    case contentTooShort:
+      return 'Inhalt zu kurz. Überprüfe auf angehängte Leerzeichen oder HTML-Tags.';
+    case contentTooLong:
+      return 'Inhalt zu lang. Überprüfe auf angehängte Leerzeichen.';
   }
 
   await prisma.post.create({
@@ -38,4 +48,10 @@ export async function addPost(formData: FormData) {
   revalidatePath('/top/month');
   revalidatePath('/top/year');
   revalidatePath('/top/all');
+
+  return null;
+}
+
+export async function addPostAction(prevState: any, formData: FormData) {
+  return await addPost(formData);
 }
