36a113a0436651b6b548514bf286fe8f696cb4f6
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2023-12-05 18:19:47 +0100

    add title to switch component

--- app/_components/switch.tsx
+++ app/_components/switch.tsx
@@ -4,23 +4,24 @@ import React from 'react';
 
 export function Switch({
   label,
+  title,
   checked,
   onSwitch,
 }: {
   label: string;
+  title: string;
   checked: boolean;
   onSwitch?: (checked: boolean) => void;
 }) {
   return (
-    <label className="relative inline-flex items-center cursor-pointer">
+    <label className="relative inline-flex items-center cursor-pointer" title={title}>
       <input
-        type="checkbox"
-        value=""
-        className="sr-only peer"
         checked={checked}
+        className="sr-only peer"
         onChange={(event) => {
           onSwitch?.(event.target.checked);
         }}
+        type="checkbox"
       />
       <div className="w-11 h-6 bg-zinc-200 rounded-full peer dark:bg-zinc-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-zinc-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-zinc-700 peer-checked:bg-zinc-400"></div>
       <span className="ms-3 text-sm font-medium text-zinc-900 dark:text-zinc-300">{label}</span>
--- app/_components/switchHideUnpopularPosts.tsx
+++ app/_components/switchHideUnpopularPosts.tsx
@@ -2,6 +2,8 @@
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
+import { DEFAULTS } from '@/utils';
+
 import { Switch } from './switch';
 
 export function SwitchHideUnpopularPosts() {
@@ -9,12 +11,14 @@ export function SwitchHideUnpopularPosts() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const hideUnpopularPosts = searchParams.get('hideUnpopularPosts') !== null;
+  const downvotesToHidePost = Number(process.env.DOWNVOTES_TO_HIDE_POST ?? DEFAULTS.DOWNVOTES_TO_HIDE_POST);
 
   return (
     <Switch
-      label="Unbeliebte Posts verstecken"
       checked={hideUnpopularPosts}
+      label="Unbeliebte Posts verstecken"
       onSwitch={(checked) => router.push(pathname + (checked ? '?hideUnpopularPosts' : ''))}
+      title={`Versteckt Posts mit ${downvotesToHidePost} oder mehr Downvotes`}
     />
   );
 }
