f2094597a88b7af6c7166d67b83fbaf4103cf27d
stefanluth <stefan.luthardt@outlook.com>
2024-02-21 22:53:16 +0100

    extract submit button
    useFormStatus() somehow does not work otherwise

--- app/_components/post/reportPost.tsx
+++ app/_components/post/reportPost.tsx
@@ -6,15 +6,22 @@ import { reportPostAction } from '@/app/_actions/post/report';
 
 export function ReportPost({ postId, fingerprint }: { postId: string; fingerprint: string }) {
   const [_, formAction] = useFormState(reportPostAction, '');
-  const { pending } = useFormStatus();
 
   return (
     <form action={formAction}>
       <input type="hidden" name="postId" value={postId} />
       <input type="hidden" name="fingerprint" value={fingerprint} />
-      <button className="hover:underline" type="submit" title="Report Post">
-        {pending ? '...' : 'Report'}
-      </button>
+      <SubmitButton />
     </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="hover:underline" type="submit" title="Report Post">
+      {pending ? '...' : 'Report'}
+    </button>
   );
 }
