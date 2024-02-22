93c5e64b6738024c68ca0838a1d54281a135a15a
stefanluth <stefan.luthardt@outlook.com>
2024-02-22 21:11:06 +0100

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,21 @@ export const metadata: Metadata = {
   description: 'Anonymous message board to ask questions, express opinions, and give feedback without outing yourself.',
 };
 
+const EMOJIS = ['❤️', '💜', '🫶', '🚀', '👽', '🤖'];
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col max-h-screen min-w-[275px]">
           <NavBar />
-          <div className="overflow-y-auto md:pb-4 md:px-2" tabIndex={-1}>
-            <div className="sm:max-w-6xl flex flex-col mx-auto">{children}</div>
+          <div className="overflow-y-auto" tabIndex={-1}>
+            <div className="sm:max-w-6xl flex flex-col mx-auto md:pb-4 md:px-2">{children}</div>
+            <footer className="flex pb-3 pt-2 pr-4 justify-end tracking-wide text-sm text-zinc-400 border-zinc-700 border-t">
+              <a href="https://github.com/stefanluth/kummerkasten">
+                Made with {EMOJIS[Math.floor(Math.random() * EMOJIS.length)]}
+              </a>
+            </footer>
           </div>
         </div>
       </body>
